7f2ea4b4f96b09d24f815314f20a0ab7f9b8ea1f
bmccurry <bmccurry@stcharlessd.org>
2014-09-26 04:04:55 -0500

    modified:   Guess_My_Number.py

--- Guess_My_Number.py
+++ Guess_My_Number.py
@@ -18,15 +18,18 @@ print("Try to guess it in as few attempts as possible.\n")
 
 # set the initial values
 the_number = random.randint(1, 100)
-guess = # Create the priming read here
+guess = input("Guess: ")# Create the priming read here
 tries = 1
 
 # guessing loop
 while guess != the_number:
     #put the guessing game logic here
-            
+    if guess > the_number:
+        print("Too high")
+    elif guess < the_number:
+        print("Too low")
     tries += 1
-    guess = #Create the following read here
+    guess = input("Guess: ") #Create the following read here
 
 
 print("You guessed it!  The number was", the_number)
